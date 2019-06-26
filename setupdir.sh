a265583ba82c590f671f273d9a509a3df98a397b
zhongzhizhao <1104@mapgoo.net>
2019-06-26 14:47:39 +0800

    setup sh file.

---
+++ setupdir.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+echo "Hello World!"
+echo "Begin run script file..."
+
+echo "To mkdir."
+
+mkdir /var/local/www
+mkdir /var/local/www/flaskscan
+mkdir /var/local/www/logs
+mkdir /var/local/www/flaskscan/uwsgi
+touch /var/local/www/logs/flaskscan.log
+
+echo "The end."
